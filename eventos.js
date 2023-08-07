90eb84bf26ce8939a7c95f5ca02340a5fc2a2423
Agustín Guerrero <110061441+agusgc@users.noreply.github.com>
2023-08-07 19:18:36 -0300

    solucion-click

--- eventos.js
+++ eventos.js
@@ -1,5 +1,10 @@
 const contenedor = document.querySelector("div");
+const boton = document.querySelector("button");
 
 contenedor.addEventListener("click", () => {
     alert("Hola! Soy el div");
+  });
+
+boton.addEventListener("click", (event) => {
+    event.stopPropagation();
   });
